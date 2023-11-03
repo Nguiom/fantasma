d09eb78dbe49f7bda4dfb004cfaa4df4f7056b07
simple <nguiom@unal.edu.co>
2023-11-03 12:15:49 -0500

    a

--- scripts/move.py
+++ scripts/move.py
@@ -3,30 +3,48 @@
 import rospy
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
+from dynamixel_workbench_msgs.srv import DynamixelCommand
+import numpy as np
 
 class Moving(): 
 	def __init__(self): 
 		rospy.init_node('Moving', anonymous=False)
-		self.cmd_vel = rospy.Publisher('/turtle1/cmd_vel',Twist, queue_size=10) 
 		self.key_sub = rospy.Subscriber('key',String, self.judge)
-		self.vel=Twist()
 		rate=rospy.Rate(10)
-		self.tabla={'w':(1,0),'s':(-1,0),'a':(0,1),'d':(0,-1)}
-		while not rospy.is_shutdown():
-			self.cmd_vel.publish(self.vel)
-			rate.sleep()
+		self.key=0
+		self.name='a'
+		self.tabla={'1':self.interpret(180,180,180,180),'2':self.interpret(205,205,200,160),\
+			'3':self.interpret(145,215,150,210),'4':self.interpret(260,160,105,205),\
+				'5':self.interpret(260,145,105,165)}
+	def jointCommand(command, id_num, addr_name, value, time):
+		rospy.wait_for_service('dynamixel_workbench/dynamixel_command')
+		try:        
+			dynamixel_command = rospy.ServiceProxy(
+            	'/dynamixel_workbench/dynamixel_command', DynamixelCommand)
+			result = dynamixel_command(command,id_num,addr_name,value)
+			rate.sleep(time)
+			return result.comm_result
+		except rospy.ServiceException as exc:
+				print(str(exc))
 	def judge(self,data):
-		temp=self.tabla.get(data.data,(0,0))
+		self.key=self.tabla.get(data.data,(0,0,0,0))
+
 		rospy.loginfo(temp)
-		vx,wz=temp
-		self.vel.linear.x=vx
-		self.vel.linear.y=0
-		self.vel.linear.z=0
-		self.vel.angular.x=0
-		self.vel.angular.y=0
-		self.vel.angular.z=wz
+	def interpret(self,a,b,c,d):
+		ans=[]
+		ans.append(np.interp(a,[0,300],[0,1022]))
+		ans.append(np.interp(b,[0,300],[0,1022]))
+		ans.append(np.interp(c,[0,300],[0,1022]))
+		ans.append(np.interp(d,[0,300],[0,1022]))
+		return ans
 if __name__== "__main__": 
 	try:
-		Moving() 
+		temp=Moving()
+		temp.jointCommand('', 1, 'Torque_Limit', temp.key[0], 0.5)
+		temp.jointCommand('', 2, 'Torque_Limit', temp.key[0], 0.5)
+		temp.jointCommand('', 3, 'Torque_Limit', temp.key[0], 0.5)
+		temp.jointCommand('', 4, 'Torque_Limit', temp.key[0], 0.5)
+		while not rospy.is_shutdown():
+			temp.jointCommand('',1,'Goal_Position',)
 	except: 
 		rospy.loginfo("End of the trip for Turtlesim") 
